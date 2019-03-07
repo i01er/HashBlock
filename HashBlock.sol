09989d5cc770215e596af4db9e01cd0afb87f53d
i01er <free2737@gmail.com>
2019-03-07 17:44:42 +0800

    Add files via upload

--- HashBlock.sol
+++ HashBlock.sol
@@ -2,25 +2,25 @@ pragma solidity ^0.5.0;
 
 contract HashBlock
 {
-	address public OwnerAddress = msg.sender;
+	address public OwnerAddress = msg.sender;								//Set the contract owner when deploy
 	uint public hash;
 
 	modifier OnlyOwner {
-		if(msg.sender == OwnerAddress)_;
+		if(msg.sender == OwnerAddress)_;									//Only The contract owner
 	}
 
-	function ChangeOwner(address _newOwner) public OnlyOwner
+	function ChangeOwner(address _newOwner) public OnlyOwner				//Transfer the ownership to other address
 	{
-		if(_newOwner == address(0))revert("The new address is null.");
+		if(_newOwner == address(0))revert("The new address is null.");		//Check the address is not null(0x0)
 		OwnerAddress = _newOwner;
 	}
 
-	function UploadHash(uint _hash) public
+	function UploadHash(uint _hash) public									//Upload the hash
 	{
 		hash = _hash;
 	}
 	
-	function Get() view public returns (uint) {
+	function Get() view public returns (uint) {								//View the hash by calling this function, callback do not need gas
 		return hash;
     }
 }
